1fc986b5baf854c1aedd2089116b464c9ff509f9
nwuensche <Wuensche.Niklas@gmail.com>
2019-03-12 20:19:13 +0100

    Add License

--- update_reminder.sh
+++ update_reminder.sh
@@ -2,7 +2,6 @@
 MARKT_LAST_CHANGE=$(wget -q -O- https://wo-ist-markt.de/cities/berlin.json | sed -n 's/.*aktualisiert am \(.*\)",/\1/p')
 BERLIN_LAST_CHANGE=$(wget -q -O- https://daten.berlin.de/datensaetze/wochen-und-tr%C3%B6delm%C3%A4rkte-0 | sed -n '/.*Aktualisiert.*/,$p' | sed -e '/.*end.*/,$d' | sed -n 's/.*<span .*>\(.*\)<\/span.*/\1/p')
 
-echo "Test";
 if [[ "$MARKT_LAST_CHANGE" != "$BERLIN_LAST_CHANGE" ]]; then
   echo "Dates don't match!"
   echo "daten.berlin.de: $MARKT_LAST_CHANGE"
