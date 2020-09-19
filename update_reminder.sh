fa8a47c147d0f3a98b3e3b2984aa1d6bc27c228c
Tobias Preuss <tobias.preuss@googlemail.com>
2020-09-19 12:46:21 +0200

    Fix POSIX error.
    + Error: "In POSIX sh, [[ ]] is undefined."

--- update_reminder.sh
+++ update_reminder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 MARKT_LAST_CHANGE=$(wget -q -O- https://wo-ist-markt.de/cities/berlin.json | sed -n 's/.*aktualisiert am \(.*\)",/\1/p')
 BERLIN_LAST_CHANGE=$(wget -q -O- https://daten.berlin.de/datensaetze/wochen-und-tr%C3%B6delm%C3%A4rkte-0 | sed -n '/.*Aktualisiert.*/,$p' | sed -e '/.*end.*/,$d' | sed -n 's/.*<span .*>\(.*\)<\/span.*/\1/p')
 
