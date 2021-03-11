914c78beb1953232a22633a9390c954113821a0d
Knut Hühne <knut@k-nut.eu>
2021-03-11 21:19:37 +0100

    Update URL for upstream data
    There was a big update of the daten.berlin.de which also slightly
    changed the url under which this dataset is made available.

--- update_reminder.sh
+++ update_reminder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MARKT_LAST_CHANGE=$(wget -q -O- https://wo-ist-markt.de/cities/berlin.json | sed -n 's/.*aktualisiert am \(.*\)",/\1/p')
-BERLIN_LAST_CHANGE=$(wget -q -O- https://daten.berlin.de/datensaetze/wochen-und-tr%C3%B6delm%C3%A4rkte-0 | sed -n '/.*Aktualisiert.*/,$p' | sed -e '/.*end.*/,$d' | sed -n 's/.*<span .*>\(.*\)<\/span.*/\1/p')
+BERLIN_LAST_CHANGE=$(wget -q -O- https://daten.berlin.de/datensaetze/wochen-und-trödelmärkte | sed -n '/.*Aktualisiert.*/,$p' | sed -e '/.*end.*/,$d' | sed -n 's/.*<span .*>\(.*\)<\/span.*/\1/p')
 
 if [[ "$MARKT_LAST_CHANGE" != "$BERLIN_LAST_CHANGE" ]]; then
   echo "Dates don't match!"
